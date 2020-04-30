a0cc00a7f972932e886e34152bfc1f7cc67de92d
Julia Olmstead <jl260629@dal.ca>
2020-04-30 04:24:55 -0300

    add uniqueChars HashSet and beginnings of a sort

--- Problem_1/src/Tweet.java
+++ Problem_1/src/Tweet.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Tweet{
     public static void main (String [] args){
@@ -17,8 +16,22 @@ public class Tweet{
         for(int i = 0; i < inputtedString.length(); i++){
             stringList.add(inputtedString.charAt(i));
         }
+        //System.out.println(stringList); //simple check
 
+        //unique characters only list:
+        Set uniqueChars = new HashSet(stringList);
+        //System.out.println("uniqueList : " + uniqueChars); //simple check
+        ArrayList uniqueCharsList = new ArrayList(uniqueChars);
+
+        //sorted array
+        ArrayList sortedArray = new ArrayList<>();
         //Count the amount of time a letter shows up
+        for(int i =0; i < uniqueChars.size(); i++){
+            int count = Collections.frequency(stringList, uniqueCharsList.get(i));
+            if(count > 1){
+
+            }
+        }
 
 
     }
