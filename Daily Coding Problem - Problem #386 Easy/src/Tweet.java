d11cb88ab8b85de388d0e68a362fd6d1723f8466
Julia Olmstead <jl260629@dal.ca>
2020-04-30 22:46:10 -0300

    rename file and update of some code

--- Daily Coding Problem - Problem #386 Easy/src/Tweet.java
+++ Daily Coding Problem - Problem #386 Easy/src/Tweet.java
@@ -5,13 +5,9 @@ public class Tweet{
 
         //instantiate the scanner
         Scanner in = new Scanner(System.in);
-
         //read in the string
         String inputtedString = in.nextLine();
-
-        //add string to list
         ArrayList stringList = new ArrayList<>();
-
         //add each character of the string to the array list
         for(int i = 0; i < inputtedString.length(); i++){
             stringList.add(inputtedString.charAt(i));
@@ -23,15 +19,25 @@ public class Tweet{
         //System.out.println("uniqueList : " + uniqueChars); //simple check
         ArrayList uniqueCharsList = new ArrayList(uniqueChars);
 
-        //sorted array
-        ArrayList sortedArray = new ArrayList<>();
-        //Count the amount of time a letter shows up
-        for(int i =0; i < uniqueChars.size(); i++){
-            int count = Collections.frequency(stringList, uniqueCharsList.get(i));
-            if(count > 1){
-
-            }
+        //create hash map where key is the letter and value is the frequency that it appears
+        HashMap frequency = new HashMap();
+        for (int i = 0; i < uniqueChars.size(); i++){
+            frequency.put(uniqueCharsList.get(i), Collections.frequency(stringList,uniqueCharsList.get(i)));
         }
+
+        //create a new list
+        ArrayList sortedArray = new ArrayList<>();
+
+        // adding the key with the largest value v amount of times to the list
+
+        for(int i = 0; i < stringList.size(); i++){
+            for(int j = 0; i < )
+            sortedArray.add();
+        }
+
+
+
+
 
 
     }
